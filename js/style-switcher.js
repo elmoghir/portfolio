5094657a928aa69de1c5077a46b51a4c5e2a124f
abdessamado <a.elmoghir@fullwaveagency.com>
2024-04-01 15:34:31 +0000

    colors

---
+++ js/style-switcher.js
@@ -0,0 +1,30 @@
+/*  toggle style switcher */
+
+const StyleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+StyleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+/* hide style - switcher o scroll */
+window.addEventListener('scroll', () =>
+{
+    if( document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+/* theme colors */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled","true");
+        }
+    })
+}
