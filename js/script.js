f6ab735d7627cc285d594e59f340f384dc59c4f6
abdessamado <a.elmoghir@fullwaveagency.com>
2024-04-01 16:01:43 +0000

    Light and dark mode

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* typing animate */
+
+var typed = new Typed(".typing", {
+    strings: ["Web Developer","Drupal Developer", "Freelancer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -28,3 +28,22 @@ function setActiveStyle(color)
         }
     })
 }
+/* theme light and dark */
+const DayNight = document.querySelector(".day-night");
+DayNight.addEventListener("click",() => {
+    DayNight.querySelector("i").classList.toggle("fa-sun");
+    DayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        DayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        DayNight.querySelector("i").classList.add("fa-moon");
+
+    }
+})
